978e833c3d65839544d54799626a022aa469a797
al-mutarjim <55210946+al-mutarjim@users.noreply.github.com>
2021-10-08 14:44:12 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    bundle: path.resolve(__dirname, "index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html|svelte)$/,
+        use: "svelte-loader",
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+              sassOptions: {
+                includePaths: ["./theme", "./node_modules"],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
